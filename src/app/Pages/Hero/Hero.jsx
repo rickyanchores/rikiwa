450110d693a283778bb0afcdae812574e0369c7d
RickyAnchores <ricky23anchores@gmail.com>
2024-08-16 10:01:02 +0200

    update

--- src/app/Pages/Hero/Hero.jsx
+++ src/app/Pages/Hero/Hero.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 const Hero = () => {
   return (
     <div className='Hero min-h-screen grid justify-center items-center p-4 text-white' id='Hero'>
         <div className="title">
-            <h1 className='text-8xl'>rikiwa.</h1>
-            <p className='text-red-600'>watch the time</p>
+            <div className="main-title flex justify-center items-center">
+             <h1 className='text-8xl'>rikiwa.</h1>
+             <div className="logo bg-red-700 p-4 m-4"><AccessTimeIcon sx={{fontSize: 60}}/></div>
+            </div>
+            <p className='text-red-600 font-semibold'>watch enthusiast blog</p>
         </div>
     </div>
   )
